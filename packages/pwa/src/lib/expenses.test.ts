926c021e768af4552f40028dcaf3f0642a1bd041
Horus Lugo <horusgoul@gmail.com>
2024-07-15 22:27:07 +0200

    Add a few utils

---
+++ packages/pwa/src/lib/expenses.test.ts
@@ -0,0 +1,365 @@
+// eslint-disable-next-line import/no-named-as-default
+import Dinero from "dinero.js";
+import { describe, test, expect } from "vitest";
+import {
+  calculateLogStatsBetweenTwoUsers,
+  calculateLogStatsOfUser,
+  type ExpenseInput,
+} from "./expenses";
+
+describe("calculateLogStatsBetweenTwoUsers", () => {
+  test("should return diffUnsplitted=0 when users spend the same amount", () => {
+    const user = "user1";
+    const otherUser = "user2";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user1",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 200,
+        paidBy: "user1",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 300,
+        paidBy: "user2",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    const result = calculateLogStatsBetweenTwoUsers(user, otherUser, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(0);
+  });
+
+  test("should return diffUnsplitted=-100 when expenses splits are 50%, user1 paid 100, and user1 paid 300", () => {
+    const user = "user1";
+    const otherUser = "user2";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user1",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 300,
+        paidBy: "user2",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    const result = calculateLogStatsBetweenTwoUsers(user, otherUser, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(-100);
+  });
+
+  test("should return diffUnsplitted=100 when expenses splits are 50%, user1 paid 300, and user2 paid 100", () => {
+    const user = "user1";
+    const otherUser = "user2";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 300,
+        paidBy: "user1",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user2",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    const result = calculateLogStatsBetweenTwoUsers(user, otherUser, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(100);
+  });
+
+  test("should return diffUnsplitted=0 when expenses are splitted evenly", () => {
+    const user = "user1";
+    const otherUser = "user2";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 200,
+        paidBy: "user1",
+        paidFor: { user1: 0.75, user2: 0.25 },
+      },
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user2",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    const result = calculateLogStatsBetweenTwoUsers(user, otherUser, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(0);
+  });
+
+  test("should return diffUnsplitted=-50 when user1 pays something for themselves", () => {
+    const user = "user1";
+    const otherUser = "user2";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 200,
+        paidBy: "user1",
+        paidFor: { user1: 1 },
+      },
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user2",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    const result = calculateLogStatsBetweenTwoUsers(user, otherUser, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(-50);
+  });
+
+  test("should return diffUnsplitted=50 when user1 pays something for themselves", () => {
+    const user = "user2";
+    const otherUser = "user1";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 200,
+        paidBy: "user1",
+        paidFor: { user1: 1 },
+      },
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user2",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    const result = calculateLogStatsBetweenTwoUsers(user, otherUser, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(50);
+  });
+
+  test("should return diffUnsplitted=0 when user2 pays a debt to user1", () => {
+    const user = "user1";
+    const otherUser = "user2";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 50,
+        paidBy: "user2",
+        paidFor: { user1: 1, user2: 0 },
+      },
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user1",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    let result = calculateLogStatsBetweenTwoUsers(user, otherUser, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(0);
+
+    result = calculateLogStatsBetweenTwoUsers(otherUser, user, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(0);
+  });
+
+  test("should return diffUnsplitted=-1667 for user2, diffUnsplitted=3333 for user3", () => {
+    const user1 = "user1";
+    const user2 = "user2";
+    const user3 = "user3";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 10000,
+        paidBy: user2,
+        paidFor: { [user1]: 0.5, [user2]: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 10000,
+        paidBy: user1,
+        paidFor: {
+          [user1]: 0.3333333333333333,
+          [user2]: 0.3333333333333333,
+          [user3]: 0.3333333333333333,
+        },
+      },
+    ];
+
+    let result = calculateLogStatsBetweenTwoUsers(user1, user2, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(-1667);
+
+    result = calculateLogStatsBetweenTwoUsers(user1, user3, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(3333);
+  });
+
+  test("should return diffUnsplitted=3333 for user2, diffUnsplitted=3333 for user3", () => {
+    const user1 = "user1";
+    const user2 = "user2";
+    const user3 = "user3";
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 10000,
+        paidBy: user2,
+        paidFor: { [user1]: 0.5, [user2]: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 5000,
+        paidBy: user1,
+        paidFor: {
+          [user1]: 0,
+          [user2]: 1,
+        },
+      },
+      {
+        version: 1,
+        expense: 10000,
+        paidBy: user1,
+        paidFor: {
+          [user1]: 0.3333333333333333,
+          [user2]: 0.3333333333333333,
+          [user3]: 0.3333333333333333,
+        },
+      },
+    ];
+
+    let result = calculateLogStatsBetweenTwoUsers(user1, user2, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(3333);
+
+    result = calculateLogStatsBetweenTwoUsers(user1, user3, expenses);
+
+    expectDinero(result.diffUnsplitted).toEqual(3333);
+  });
+});
+
+describe("calculateLogStatsOfUser", () => {
+  test("should owe 0 to user2, and 100 to user3", () => {
+    const user = "user1";
+    const otherUsers = ["user2", "user3"];
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user1",
+        paidFor: { user1: 0.33333333, user2: 0.33333333, user3: 0.33333333 },
+      },
+      {
+        version: 1,
+        expense: 200,
+        paidBy: "user1",
+        paidFor: { user1: 0.33333333, user2: 0.33333333, user3: 0.33333333 },
+      },
+      {
+        version: 1,
+        expense: 300,
+        paidBy: "user2",
+        paidFor: { user1: 0.33333333, user2: 0.33333333, user3: 0.33333333 },
+      },
+      {
+        version: 1,
+        expense: 600,
+        paidBy: "user3",
+        paidFor: { user1: 0.33333333, user2: 0.33333333, user3: 0.33333333 },
+      },
+    ];
+
+    const result = calculateLogStatsOfUser(user, otherUsers, expenses);
+
+    expectDinero(result.userOwes).toEqual(100);
+    expectDinero(result.owedToUser).toEqual(0);
+
+    expectDinero(result.diffs.user2.diffUnsplitted).toEqual(0);
+    expectDinero(result.diffs.user3.diffUnsplitted).toEqual(-100);
+  });
+
+  test("user2 should owe 2 to user1", () => {
+    const user = "user1";
+    const otherUsers = ["user2"];
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user1",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 200,
+        paidBy: "user1",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user2",
+        paidFor: { user1: 0.5, user2: 0.5 },
+      },
+    ];
+
+    const result = calculateLogStatsOfUser(user, otherUsers, expenses);
+
+    expectDinero(result.userOwes).toEqual(0);
+    expectDinero(result.owedToUser).toEqual(100);
+    expectDinero(result.diffs.user2.diffUnsplitted).toEqual(100);
+  });
+
+  test("user2 should owe 200 to user1, and user3 should owe 150 to user1, but user3 should only owe 50 to user2", () => {
+    const expenses: ExpenseInput[] = [
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user1",
+        paidFor: { user1: 0.33333333, user2: 0.33333333, user3: 0.33333333 },
+      },
+      {
+        version: 1,
+        expense: 200,
+        paidBy: "user1",
+        paidFor: { user1: 0.33333333, user2: 0.33333333, user3: 0.33333333 },
+      },
+      {
+        version: 1,
+        expense: 100,
+        paidBy: "user2",
+        paidFor: { user1: 0.33333333, user2: 0.33333333, user3: 0.33333333 },
+      },
+    ];
+
+    let result = calculateLogStatsOfUser("user1", ["user2", "user3"], expenses);
+
+    expectDinero(result.userOwes).toEqual(0);
+    expectDinero(result.owedToUser).toEqual(167);
+    expectDinero(result.diffs.user2.diffUnsplitted).toEqual(67);
+
+    result = calculateLogStatsOfUser("user3", ["user2", "user1"], expenses);
+
+    expectDinero(result.userOwes).toEqual(133);
+    expectDinero(result.owedToUser).toEqual(0);
+    expectDinero(result.diffs.user2.diffUnsplitted).toEqual(-33);
+  });
+});
+
+function expectDinero(dinero: Dinero.Dinero) {
+  return expect(dinero.getAmount());
+}
---
+++ packages/pwa/src/lib/expenses.ts
@@ -0,0 +1,113 @@
+// eslint-disable-next-line import/no-named-as-default
+import Dinero from "dinero.js";
+
+export type ExpenseUser = string;
+export interface ExpenseInput {
+  version: 1;
+  paidBy: string;
+  paidFor: Record<string, number>;
+  expense: number;
+}
+
+export function calculateLogStatsBetweenTwoUsers(
+  userUid: ExpenseUser,
+  otherUserUid: ExpenseUser,
+  expenses: ExpenseInput[],
+) {
+  const groupedExpenses = expenses
+    .filter(
+      (expense) =>
+        [userUid, otherUserUid].includes(expense.paidBy) &&
+        userUid in expense.paidFor &&
+        otherUserUid in expense.paidFor,
+    )
+    .reduce(
+      (prev, next) => {
+        const user = next.paidBy;
+        const prevExpenses = prev[user] ?? [];
+
+        return {
+          ...prev,
+          [user]: [...prevExpenses, next],
+        };
+      },
+      {} as Record<string, ExpenseInput[]>,
+    );
+
+  const {
+    [userUid]: userExpenses = [],
+    [otherUserUid]: otherUserExpenses = [],
+  } = groupedExpenses;
+
+  /**
+   * What U2 has to pay to U1
+   */
+  const whatU2HasToPayToU1 = getSplitTotal(userExpenses, otherUserUid);
+
+  /**
+   * What U1 has to pay to U2
+   */
+  const whatU1HasToPayToU2 = getSplitTotal(otherUserExpenses, userUid);
+
+  const diff = whatU2HasToPayToU1.subtract(whatU1HasToPayToU2);
+
+  return {
+    diffUnsplitted: diff,
+  };
+}
+
+type UserDiff = ReturnType<typeof calculateLogStatsBetweenTwoUsers>;
+
+export function calculateLogStatsOfUser(
+  userUid: ExpenseUser,
+  listUsers: ExpenseUser[],
+  expenses: ExpenseInput[],
+) {
+  const diffs: Record<string, UserDiff> = {};
+
+  for (const otherUserUid of listUsers.filter((uid) => uid !== userUid)) {
+    diffs[otherUserUid] = calculateLogStatsBetweenTwoUsers(
+      userUid,
+      otherUserUid,
+      expenses,
+    );
+  }
+
+  const zero = Dinero({ amount: 0 });
+
+  const userOwes = Object.values(diffs)
+    .filter((diff) => diff.diffUnsplitted.lessThan(zero))
+    .map((diff) => diff.diffUnsplitted)
+    .reduce(
+      (prev, next) => prev.add(Dinero({ amount: Math.abs(next.getAmount()) })),
+      Dinero({ amount: 0 }),
+    );
+
+  const owedToUser = Object.values(diffs)
+    .filter((diff) => diff.diffUnsplitted.greaterThan(zero))
+    .map((diff) => diff.diffUnsplitted)
+    .reduce((prev, next) => prev.add(next), Dinero({ amount: 0 }));
+
+  return {
+    userOwes,
+    owedToUser,
+    diffs,
+  };
+}
+
+function getSplitTotal(expenses: ExpenseInput[], uid: string, reverse = false) {
+  return expenses.reduce(
+    (prev, next) => {
+      let amount = Dinero({
+        amount: next.expense,
+      });
+
+      amount = amount.multiply(
+        reverse ? 1 - next.paidFor[uid] : next.paidFor[uid],
+      );
+
+      return prev.add(amount);
+    },
+    Dinero({ amount: 0 }),
+  );
+}
