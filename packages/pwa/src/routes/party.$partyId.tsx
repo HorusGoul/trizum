55df7eebe5a245d54c524e3eff62c1411e55c0ee
Mario Nachbaur <marionauta@users.noreply.github.com>
2024-07-16 19:07:19 +0200

    Load Party via ID in Party route

--- packages/pwa/src/routes/party.$partyId.tsx
+++ packages/pwa/src/routes/party.$partyId.tsx
@@ -1,5 +1,48 @@
+import { isValidDocumentId, updateText } from "@automerge/automerge-repo/slim";
+import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import { createFileRoute } from "@tanstack/react-router";
+import type { Party } from "#src/models/party.js";
 
 export const Route = createFileRoute("/party/$partyId")({
-  component: () => <div>Hello /party/$partyId!</div>,
+  component: PartyById,
 });
+
+function PartyById() {
+  const { party } = useParty();
+
+  if (party === undefined) {
+    return <span>404 bruv</span>;
+  }
+
+  return (
+    <div className="flex min-h-full flex-col">
+      <div className="container flex h-16 items-center pr-2">
+        <h1 className="pl-4 text-2xl font-bold">{party.name}</h1>
+      </div>
+    </div>
+  );
+}
+
+function useParty() {
+  const { partyId: _partyId } = Route.useParams();
+  const partyId = isValidDocumentId(_partyId) ? _partyId : undefined;
+  const [party, changeParty] = useDocument<Party>(partyId);
+  function dispatch({ type, payload }: ChangePartyAction) {
+    switch (type) {
+      case "party_change_name":
+        return changeParty((party) => {
+          updateText(party, ["name"], payload.name);
+        });
+    }
+  }
+  return { party, partyId, dispatch };
+}
+
+interface ChangePartyNameAction {
+  type: "party_change_name";
+  payload: {
+    name: string;
+  };
+}
+
+type ChangePartyAction = ChangePartyNameAction;
