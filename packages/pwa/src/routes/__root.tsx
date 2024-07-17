66e1b7457a4c143063c63874d4f7886ffa30cd18
Horus Lugo <horusgoul@gmail.com>
2024-07-17 23:45:28 +0200

    use import.meta.env.PROD instead of process.env.NODE_ENV

--- packages/pwa/src/routes/__root.tsx
+++ packages/pwa/src/routes/__root.tsx
@@ -2,46 +2,47 @@ import { createRootRoute, Outlet, useRouter } from "@tanstack/react-router";
 import * as React from "react";
 import { RouterProvider } from "react-aria-components";
 
-const TanStackRouterDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
-        // Lazy load in development
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-        })),
-      );
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    );
 
 export const Route = createRootRoute({
-  component: function RootRoute() {
-    const router = useRouter();
-
-    return (
-      <RouterProvider
-        navigate={(to, options) => {
-          if (typeof to === "string") {
-            router.navigate({ to, ...options });
-            return;
-          }
-
-          router.navigate({
-            ...to,
-            ...options,
-          });
-        }}
-        useHref={(to) => {
-          if (typeof to === "string") {
-            return router.buildLocation({ to }).href;
-          }
-
-          return router.buildLocation(to).href;
-        }}
-      >
-        <Outlet />
-        <React.Suspense fallback={null}>
-          <TanStackRouterDevtools />
-        </React.Suspense>
-      </RouterProvider>
-    );
-  },
+  component: Root,
 });
+
+function Root() {
+  const router = useRouter();
+
+  return (
+    <RouterProvider
+      navigate={(to, options) => {
+        if (typeof to === "string") {
+          router.navigate({ to, ...options });
+          return;
+        }
+
+        router.navigate({
+          ...to,
+          ...options,
+        });
+      }}
+      useHref={(to) => {
+        if (typeof to === "string") {
+          return router.buildLocation({ to }).href;
+        }
+
+        return router.buildLocation(to).href;
+      }}
+    >
+      <Outlet />
+      <React.Suspense fallback={null}>
+        <TanStackRouterDevtools />
+      </React.Suspense>
+    </RouterProvider>
+  );
+}
