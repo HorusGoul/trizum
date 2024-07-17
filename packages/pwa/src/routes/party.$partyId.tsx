df39d33fc0f18b87e559cf6f973b7b50143efa84
Mario Nachbaur <marionauta@users.noreply.github.com>
2024-07-17 11:40:42 +0200

    Add delete Party button

--- packages/pwa/src/routes/party.$partyId.tsx
+++ packages/pwa/src/routes/party.$partyId.tsx
@@ -1,14 +1,27 @@
 import { isValidDocumentId, updateText } from "@automerge/automerge-repo/slim";
 import { useDocument } from "@automerge/automerge-repo-react-hooks";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import type { Party } from "#src/models/party.js";
+import { MenuTrigger, Popover } from "react-aria-components";
+import { IconButton } from "#src/ui/IconButton.js";
+import { Menu, MenuItem } from "#src/ui/Menu.js";
+import { IconWithFallback } from "#src/ui/Icon.js";
+import { usePartyList } from "#src/hooks/usePartyList.js";
 
 export const Route = createFileRoute("/party/$partyId")({
   component: PartyById,
 });
 
 function PartyById() {
-  const { party } = useParty();
+  const { party, partyId } = useParty();
+  const { removeParty } = usePartyList();
+  const navigate = useNavigate();
+
+  function onDeleteParty() {
+    if (!partyId) return;
+    removeParty(partyId);
+    navigate({ to: "/", replace: true });
+  }
 
   if (party === undefined) {
     return <span>404 bruv</span>;
@@ -18,6 +31,18 @@ function PartyById() {
     <div className="flex min-h-full flex-col">
       <div className="container flex h-16 items-center pr-2">
         <h1 className="pl-4 text-2xl font-bold">{party.name}</h1>
+        <div className="flex-1" />
+        <MenuTrigger>
+          <IconButton icon="ellipsis-vertical" aria-label="Menu" />
+          <Popover placement="bottom end">
+            <Menu>
+              <MenuItem onAction={onDeleteParty}>
+                <IconWithFallback name="trash" size={20} className="mr-3" />
+                <span className="h-3.5 leading-none">Delete</span>
+              </MenuItem>
+            </Menu>
+          </Popover>
+        </MenuTrigger>
       </div>
     </div>
   );
