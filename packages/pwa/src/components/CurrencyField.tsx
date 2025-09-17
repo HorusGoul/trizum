1ef3869791decea8df5ad0300960b27b8223bf96
Horus Lugo <horusgoul@gmail.com>
2025-09-17 02:36:21 +0200

    Maybe this works

--- packages/pwa/src/components/CurrencyField.tsx
+++ packages/pwa/src/components/CurrencyField.tsx
@@ -1,19 +1,12 @@
-import { AppNumberField, type AppNumberFieldProps } from "#src/ui/TextField.js";
-import { use } from "react";
+import { AppCurrencyField } from "#src/ui/TextField.js";
+import React, { use } from "react";
 import { CurrencyContext } from "./CurrencyContext";
 
-export interface CurrencyFieldProps extends AppNumberFieldProps {}
+export interface CurrencyFieldProps
+  extends React.ComponentProps<typeof AppCurrencyField> {}
 
 export function CurrencyField({ ...props }: CurrencyFieldProps) {
   const currency = use(CurrencyContext);
 
-  return (
-    <AppNumberField
-      {...props}
-      formatOptions={{
-        style: "decimal",
-        currency,
-      }}
-    />
-  );
+  return <AppCurrencyField {...props} currency={currency} />;
 }
