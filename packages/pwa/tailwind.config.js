ad1f1fb12e7f51f1af4401ed93dfa5e0e0a9355e
Horus Lugo <horusgoul@gmail.com>
2025-09-18 00:58:05 +0200

    Force dark mode
    Fixes #29

--- packages/pwa/tailwind.config.js
+++ packages/pwa/tailwind.config.js
@@ -7,6 +7,7 @@ import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "selector",
   theme: {
     extend: {
       colors: {
