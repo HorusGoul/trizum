cdcd6545213976c46e1b5eaac33de65ab5e094f3
Horus Lugo <horusgoul@gmail.com>
2025-09-23 00:32:49 +0200

    remove padding from icons

--- packages/pwa/pwa-assets.config.ts
+++ packages/pwa/pwa-assets.config.ts
@@ -4,12 +4,15 @@ const preset: Preset = {
   transparent: {
     sizes: [64, 192, 512],
     favicons: [[48, "favicon.ico"]],
+    padding: 0,
   },
   maskable: {
     sizes: [512],
+    padding: 0,
   },
   apple: {
     sizes: [180],
+    padding: 0,
   },
 };
 
