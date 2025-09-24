d7f6cf742202fb9064e7ad0f6e1cf6be47b1861f
Horus Lugo <horusgoul@gmail.com>
2025-09-24 02:07:45 +0200

    rAF

--- packages/pwa/src/ui/theme.ts
+++ packages/pwa/src/ui/theme.ts
@@ -5,11 +5,19 @@ import {
 
 export const defaultThemeHue = 250;
 
+let lastRAF: number | null = null;
+
 export function setThemeHue(hue: number) {
   const variables = getVariables({
     baseName: "accent",
     hue,
   });
 
-  updateVariables(variables);
+  if (lastRAF) {
+    window.cancelAnimationFrame(lastRAF);
+  }
+
+  lastRAF = window.requestAnimationFrame(() => {
+    updateVariables(variables);
+  });
 }
