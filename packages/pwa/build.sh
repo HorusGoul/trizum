426ddde7b3ca64dea1de6b5aaa58bf9f97f077cf
Horus Lugo <horusgoul@gmail.com>
2026-01-19 00:05:57 +0100

    Exports

---
+++ packages/pwa/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export SENTRY_ORG="horusdev"
+export SENTRY_PROJECT="trizum-pwa"
+
+vite build
+
+sentry-cli sourcemaps inject ./dist/client
+
+if [ -z "$SENTRY_AUTH_TOKEN" ]; then
+  echo "SENTRY_AUTH_TOKEN is not set, skipping sourcemaps upload"
+  exit 0
+fi
+
+# Get full version
+PWA_VERSION=$(pnpm pkg get version | xargs)
+LAST_COMMIT="$(git rev-parse --short HEAD)"
+FULL_VERSION="${PWA_VERSION}-${LAST_COMMIT}"
+
+echo "Uploading sourcemaps to Sentry for release $FULL_VERSION. Org: $SENTRY_ORG, Project: $SENTRY_PROJECTS"
+
+sentry-cli sourcemaps upload \
+  --release "$FULL_VERSION" \
+  --org "$SENTRY_ORG" \
+  --project "$SENTRY_PROJECT" \
+  ./dist/client
