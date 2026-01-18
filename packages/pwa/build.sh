b41612143355b1b7916eb13087bbec89204879fc
Horus Lugo <horusgoul@gmail.com>
2026-01-19 00:08:55 +0100

    Remove s

--- packages/pwa/build.sh
+++ packages/pwa/build.sh
@@ -17,7 +17,7 @@ PWA_VERSION=$(pnpm pkg get version | xargs)
 LAST_COMMIT="$(git rev-parse --short HEAD)"
 FULL_VERSION="${PWA_VERSION}-${LAST_COMMIT}"
 
-echo "Uploading sourcemaps to Sentry for release $FULL_VERSION. Org: $SENTRY_ORG, Project: $SENTRY_PROJECTS"
+echo "Uploading sourcemaps to Sentry for release $FULL_VERSION. Org: $SENTRY_ORG, Project: $SENTRY_PROJECT"
 
 sentry-cli sourcemaps upload \
   --release "$FULL_VERSION" \
